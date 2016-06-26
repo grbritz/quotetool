b67dcf6ae470e7e55c7c8a291753e8e363fbf39a
Graeme Britz <grbritz@gmail.com>
2016-06-26 15:30:16 -0700

    tweaking db connect info

--- config/config.js
+++ config/config.js
@@ -18,7 +18,9 @@ var config = {
       name: 'quotetool'
     },
     port: 23714,
-    db: 'mongodb://heroku_8ffksphm:3ubb4jhehfhcd387il7kmjiqu9@ds023714.mlab.com/heroku_8ffksphm'
+    db: 'mongodb://heroku_8ffksphm:3ubb4jhehfhcd387il7kmjiqu9@ds023714.mlab.com/heroku_8ffksphm',
+    db_user: process.env.DB_USER,
+    db_pass: process.env.DB_PASS
   },
 
   production: {
